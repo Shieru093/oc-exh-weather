e723862289299f9767020110ff21264e9b87831b
Shieru093 <shieru093@gmail.com>
2024-03-04 18:47:15 +0900

    日付表示の追加

--- src/components/ui/skeletons.tsx
+++ src/components/ui/skeletons.tsx
@@ -2,7 +2,7 @@ export function WeatherOverviewSkelton() {
 	return (
 		<div className="bg-white border-4 px-8 py-2 rounded-xl w-1/2">
 			<p className="font-bold pb-8 text-[min(4vw,80px)] text-center text-gray-800">-</p>
-			<p className="pl-20 text-[min(4vw,24px)]">現在( hh:mm 時点 )の天気は...</p>
+			<p className="pl-20 text-[min(4vw,24px)]">現在( -/- hh:mm 時点 )の天気は...</p>
 			<div className="flex justify-center pb-4">
 				<p className="px-12 text-[min(10vw,160px)] text-gray-700">-</p>
 			</div>
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,14 +1,16 @@
 // utcを日本時間に変換する関数
 export function convertUnixTimestampToJST(utc: number): string {
-	const date = new Date(utc * 1000); // utcからdateオブジェクトを生成
-	date.setHours(date.getHours() + 9); // utcにjstの時差（９時間）を加える
+	const utcDate = new Date(utc * 1000); // utcからdateオブジェクトを生成
+	utcDate.setHours(utcDate.getHours() + 9); // utcにjstの時差（９時間）を加える
 
 	// 時と分を格納
-	const hour = date.getUTCHours().toString().padStart(2, '0');
-	const minute = date.getUTCMinutes().toString().padStart(2, '0');
+	const month = utcDate.getUTCMonth() + 1;
+	const date = utcDate.getUTCDate().toString();
+	const hour = utcDate.getUTCHours().toString().padStart(2, '0');
+	const minute = utcDate.getUTCMinutes().toString().padStart(2, '0');
 
 	// HH:mm形式の文字列で返す
-	return `${hour}:${minute}`;
+	return `${month.toString()}/${date} ${hour}:${minute}`;
 }
 
 // ChatGPTが生成した変換関数
